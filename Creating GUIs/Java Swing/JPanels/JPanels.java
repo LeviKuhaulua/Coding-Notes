4bf4f5dead5bdc7978f8c249d237a8c38a9a81fb
Levi Kuhaulua <levi.kuhaulua@outlook.com>
2023-04-19 22:52:01 -1000

    Updated JPanels file

--- Creating GUIs/Java Swing/JPanels/JPanels.java
+++ Creating GUIs/Java Swing/JPanels/JPanels.java
@@ -7,26 +7,46 @@ public class JPanels {
         JFrame aFrame = new JFrame("Learning JPanels"); 
         aFrame.setSize(750, 750);
         aFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        aFrame.setVisible(true);
         aFrame.setLayout(null); 
 
+        // Creating Labels 
+        JLabel labelOne = new JLabel(); 
+        labelOne.setText("First String"); 
+        labelOne.setBounds(0, 0, 100, 150); 
+        
+
+        JLabel labelTwo = new JLabel(); 
+        labelTwo.setText("Second String"); 
+        labelTwo.setBounds(50, 50, 100, 150); 
+
+        JLabel labelThree = new JLabel(); 
+        labelThree.setText("Third String");  
+        labelThree.setBounds(0, 0, 100, 150);
+        
         // Creating a bunch of panels 
         JPanel onePanel = new JPanel(null); 
         onePanel.setBounds(0, 0, 300, 300); 
         onePanel.setBackground(Color.MAGENTA); 
-        
         
         JPanel twoPanel = new JPanel(null); 
         twoPanel.setBounds(300, 0, 300, 300); 
         twoPanel.setBackground(Color.BLUE); 
 
         JPanel threePanel = new JPanel(null); 
-        threePanel.setBounds(0, 300, 600, 300); 
+        threePanel.setBounds(0, 300, (onePanel.getWidth() + twoPanel.getWidth()), 200); 
         threePanel.setBackground(Color.GREEN);
 
+
+        // Adding panels to the frame 
         aFrame.add(onePanel); 
         aFrame.add(twoPanel); 
         aFrame.add(threePanel); 
+
+        onePanel.add(labelOne); 
+        twoPanel.add(labelTwo); 
+        threePanel.add(labelThree); 
+
+        aFrame.setVisible(true);
 
     }
 }
